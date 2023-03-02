d2b8860434aff13b5948947933a016b77a7d3f8e
Abbie Papka <abbiepapka@gmail.com>
2023-03-01 20:20:49 -0800

    experiment service created

---
+++ src/services/experimentService.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const baseURL = '/api/experiment';
+
+const getAllExperiments = () => {
+  const request = axios.get(baseURL);
+  return request.then(response => response.data)
+}
+
+// const getExperiment = async(id) => {
+//   const response = await axios.get(`${baseURL}/`)
+// }
+
+const createExperiment = async(experiment) => {
+  const response = await axios.post(baseURL, experiment);
+  return response.data;
+}
+
+//update experiment functionality isn't yet defined in the api
+// const updateExperiment = async(id, newExperiment) => {
+//   await axios.put(`${baseURL}/${id}`, newExperiment);
+// }
+
+const deleteExperiment = async (id) => {
+  await axios.delete(`${baseURL}/${id}`);
+}
+
+
+const createVariants = async (id, variantsArr) => {
+  //manipulate array into proper object for API request
+  const response = await axios.post(`${baseURL}/${id}/variants`, variantsArr);
+  return response.data;
+}
+
+const updateVariants = async (id, variantsArr) => {
+  //manipulate array into proper object for API request
+  const response = await axios.put(`${baseURL}/${id}/variants`, variantsArr);
+  return response.data;
+}
+
+const experimentService = { 
+  getAllExperiments, 
+  createExperiment,
+  deleteExperiment,
+  createVariants,
+  updateVariants
+
+}
+
+export default experimentService;
