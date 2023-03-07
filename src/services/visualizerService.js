4f1a4740173acd59f8c1b4d73d7f65a774f8fb46
ChelseaSaunders <c.c.saunders@protonmail.com>
2023-03-07 08:34:33 -0800

    First test passes! Co-authored-by: Alison Martinez <alison.a.martinez@outlook.com> Co-authored-by: apapka <abbiepapka@gmail.com> Co-authored-by: Sarah Bunker <chemencoder@gmail.com>

--- src/services/visualizerService.js
+++ src/services/visualizerService.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000/api';
 
-const getExperimentEventData = async(experimentId) => {
+const getExperimentEventData = async (experimentId) => {
   const response = await axios.get(`${baseURL}/events/experiment/${experimentId}/variants/event_data`);
-  console.log("event data in service:", response.data);
+  console.log("event data in service:", response);
   return response.data;
 };
 
---
+++ src/services/visualizerService.test.js
@@ -0,0 +1,63 @@
+import visualizerService from "./visualizerService";
+import axios from "axios";
+
+jest.mock("axios");
+
+test("returns response data when valid experiment id is passed", async () => {
+  const res = {
+    data: [
+      {
+        id: 1,
+        value: "red",
+        experiment_id: 1,
+        is_control: true,
+        weight: 0.5,
+        event_total: 300,
+        distinct_user_events_total: 100,
+        total_users: 150
+      },
+      {
+        id: 2,
+        value: "blue",
+        experiment_id: 1,
+        is_control: false,
+        weight: 0.5,
+        event_total: 250,
+        distinct_user_events_total: 75,
+        total_users: 150
+      },
+    ],
+    status: 200,
+    statusText: "OK",
+    headers: {},
+    config: {},
+    request: XMLHttpRequest
+  };
+
+  const experimentId = res.data[0].experimentId;
+  const data = [
+    {
+      id: 1,
+      value: "red",
+      experiment_id: 1,
+      is_control: true,
+      weight: 0.5,
+      event_total: 300,
+      distinct_user_events_total: 100,
+      total_users: 150
+    },
+    {
+      id: 2,
+      value: "blue",
+      experiment_id: 1,
+      is_control: false,
+      weight: 0.5,
+      event_total: 250,
+      distinct_user_events_total: 75,
+      total_users: 150
+    },
+  ];
+
+  axios.get.mockResolvedValueOnce(res);
+  expect(visualizerService.getExperimentEventData()).resolves.toEqual(data);
+});
