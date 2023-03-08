5e3d45bb9c44a7503e66a00da5839a958260d93f
ChelseaSaunders <c.c.saunders@protonmail.com>
2023-03-07 19:42:36 -0800

    Future List  working; not hooked up to sidenav yet Co-authored-by: Alison Martinez <alison.a.martinez@outlook.com> Co-authored-by: apapka <abbiepapka@gmail.com> Co-authored-by: Sarah Bunker <chemencoder@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,44 +1,99 @@
 import { useEffect, useState } from 'react';
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import ExperimentForm from './components/ExperimentForm';
-import Home from './components/Home'
 import experimentService from './services/experimentService';
-import Visualizer from './components/visualizer/Visualizer'
-
+import ScheduledList from './components/SheduledList';
 import SideNav from './components/SideNav';
 
-function App() {
+const App = () => {
   const [experiments, setExperiments] = useState([]);
+  const [currentToggles, setCurrentToggles] = useState([]);
+  const [currentRollOuts, setCurrentRollOuts] = useState([]);
+  const [currentExperiments, setCurrentExperiments] = useState([]);
+  const [scheduledFeatures, setScheduledFeatures] = useState([]);
+  const [pastExperiments, setPastExperiments] = useState([]);
+
+  const parseExperiments = (experiments) => {
+    let currToggles = [];
+    let currRollOuts = [];
+    let currExperiments = [];
+    let scheduled = [];
+    let past = [];
+
+    const currentDate = new Date();
+
+    experiments.forEach(obj => {
+      const startDate = new Date(obj.start_date);
+      const endDate = new Date(obj.end_date);
+      if (currentDate >= startDate && currentDate <= endDate) {
+        switch(obj.type_id) {
+          case 1: {
+            currToggles.push(obj);
+            break;
+          } case 2: {
+            currRollOuts.push(obj);
+            break;
+          } case 3: {
+            currExperiments.push(obj);
+            break;
+          }
+        }
+      } else if (currentDate < startDate) {
+        scheduled.push(obj)
+      } else if (currentDate > endDate && obj.type_id === 3) {
+        past.push(obj);
+      }
+    })
+
+    setCurrentToggles(currToggles);
+    setCurrentRollOuts(currRollOuts);
+    setCurrentExperiments(currExperiments);
+    setScheduledFeatures(scheduled);
+    setPastExperiments(past);
+  }
 
   useEffect(()=> {
     experimentService
       .getAllExperiments()
-      .then(experiments => {
-        console.log(experiments);
-        setExperiments(experiments);
-      })
+      .then(response => {
+        setExperiments(response);
+        parseExperiments(response);
+      });
   }, [])
 
+  // console.log("currentToggles: ", currentToggles);
+  // console.log("currentRollouts: ", currentRollOuts);
+  // console.log("currentExperiments: ", currentExperiments);
+  // console.log("scheduledFeatures: ", scheduledFeatures);
+  // console.log("pastExperiments: ", pastExperiments);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/newExperiment" element={<ExperimentForm/>}/>
-        <Route path="/visualizer/:experimentId" element={<Visualizer />} />
-      </Routes>
-      <SideNav />
-      {/* <Visualizer /> */}
-    </BrowserRouter>
-    // <div>
-    //   <h1>Admin UI</h1>
-    //   <ExperimentForm/>
-    // </div>
-    // <div className="test">
-    //   <h1>Test</h1>
-    //   <ExperimentForm></ExperimentForm>
-    // </div>
+    // <SideNav />
+    <ScheduledList scheduledFeatures={scheduledFeatures} />
   );
 }
 
 export default App;
+
+
+// import ExperimentForm from './components/ExperimentForm';
+// import Home from './components/Home'
+// import Visualizer from './components/visualizer/Visualizer'
+// import {BrowserRouter, Routes, Route } from 'react-router-dom';
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home/>}/>
+    //     <Route path="/newExperiment" element={<ExperimentForm/>}/>
+    //     <Route path="/visualizer/:experimentId" element={<Visualizer />} />
+    //   </Routes>
+    //   {/* <SideNav /> */}
+    //   <ScheduledList />
+    //   {/* <Visualizer /> */}
+    // </BrowserRouter>
+    // // <div>
+    // //   <h1>Admin UI</h1>
+    // //   <ExperimentForm/>
+    // // </div>
+    // // <div className="test">
+    // //   <h1>Test</h1>
+    // //   <ExperimentForm></ExperimentForm>
+    // // </div>
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -14,36 +14,24 @@ import {
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Home', href: '#', icon: HomeIcon, current: true },
+  { name: 'Create New Feature', href: '#', icon: UsersIcon, current: false },
+  { name: 'Current Toggles', href: '#', icon: FolderIcon, current: false },
+  { name: 'Current Roll-Outs', href: '#', icon: FolderIcon, current: false },
+  { name: 'Current Experiments', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Scheduled Features', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Past Experiments', href: '#', icon: InboxIcon, current: false },
 ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function SideNav() {
+const SideNav = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="relative z-40 lg:hidden" onClose={setSidebarOpen}>
@@ -168,7 +156,6 @@ export default function SideNav() {
           </div>
         </div>
         <div className="flex flex-col lg:pl-64">
-          <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
             <button
               type="button"
               className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 lg:hidden"
@@ -178,76 +165,6 @@ export default function SideNav() {
               <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
             </button>
             <div className="flex flex-1 justify-between px-4">
-              <div className="flex flex-1">
-                <form className="flex w-full lg:ml-0" action="#" method="GET">
-                  <label htmlFor="search-field" className="sr-only">
-                    Search
-                  </label>
-                  <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                      <MagnifyingGlassIcon className="h-5 w-5" aria-hidden="true" />
-                    </div>
-                    <input
-                      id="search-field"
-                      className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 focus:border-transparent focus:outline-none focus:ring-0 focus:placeholder:text-gray-400 sm:text-sm"
-                      placeholder="Search"
-                      type="search"
-                      name="search"
-                    />
-                  </div>
-                </form>
-              </div>
-              <div className="ml-4 flex items-center lg:ml-6">
-                <button
-                  type="button"
-                  className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {userNavigation.map((item) => (
-                        <Menu.Item key={item.name}>
-                          {({ active }) => (
-                            <a
-                              href={item.href}
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
           </div>
 
           <main className="flex-1">
@@ -255,7 +172,7 @@ export default function SideNav() {
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
               </div>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{/* Your content */}</div>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">Test</div>
             </div>
           </main>
         </div>
@@ -263,3 +180,5 @@ export default function SideNav() {
     </>
   )
 }
+
+export default SideNav;
