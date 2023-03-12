7a665111d6581d6a66a9529682013557dd39470b
ChelseaSaunders <c.c.saunders@protonmail.com>
2023-03-11 17:39:37 -0800

    Lists done except for toggle pause and edit Co-authored-by: Alison Martinez <alison.a.martinez@outlook.com> Co-authored-by: apapka <abbiepapka@gmail.com> Co-authored-by: Sarah Bunker <chemencoder@gmail.com>

--- src/components/lists/PastExperimentsList.js
+++ src/components/lists/PastExperimentsList.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import listUtils from '../../utils/listUtils';
 import DeleteAlert from '../DeleteAlert';
+import DescriptionDisplay from './DescriptionDisplay';
 
 const PastExperimentsList = ({ pastFeatures, setpastFeatures, setExperimentChange }) => {
   const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
@@ -44,7 +45,7 @@ const PastExperimentsList = ({ pastFeatures, setpastFeatures, setExperimentChang
       </div>
     </div>
       <p className="mt-2 max-w-4xl text-sm text-gray-500">
-      View your past experiments and experiment analysis.
+      View your past experiments and experiment analysis, ordered by start date.
       </p>
     <div className="px-4 sm:px-6 lg:px-8">
       <div className="sm:flex sm:items-center">
@@ -55,66 +56,59 @@ const PastExperimentsList = ({ pastFeatures, setpastFeatures, setExperimentChang
             { emptyList
             ? <h3 className="text-base font-semibold leading-6 text-gray-900">You do not currently have any past experiments to display.</h3>
             : <table className="min-w-full divide-y divide-gray-300">
-              <thead>
-                <tr>
-                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
-                    Name
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    Start Date
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    End Date
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  </th>
-                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  </th>
-                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
-                    <span className="sr-only">Edit</span>
-                  </th>
+            <thead>
+              <tr>
+                <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                  Name
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                  Start Date
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                  End Date
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                </th>
+                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                  <span className="sr-only">Repeat Experiment</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {processed.map((featureObj, idx) => (
+                <tr key={featureObj.id} className={idx % 2 === 0 ? undefined : 'bg-gray-50'}>
+                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                    <DescriptionDisplay
+                      name={featureObj.name}
+                      description={featureObj.description || "No description provided."}
+                      rowLength={50}
+                      type={featureObj.type_id}
+                      id={featureObj.id}
+                      featuresArr={processed}
+                    />
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{featureObj.startDate}</td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{featureObj.endDate}</td>
+                  <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                      Repeat Experiment<span className="sr-only"></span>
+                    </a>
+                  </td>
+                  <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                    <button
+                      type="button"
+                      className="rounded bg-indigo-600 py-1 px-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      onClick={() => handleDelete(featureObj.id, pastFeatures, setpastFeatures, setError)}
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody className="bg-white">
-                {processed.map((featureObj, idx) => (
-//  Make name clickable, route to experiment vairant details
-                  <tr key={featureObj.id} className={idx % 2 === 0 ? undefined : 'bg-gray-50'}>
-                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      {featureObj.name}
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{featureObj.startDate}</td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{featureObj.endDate}</td>
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-{/* need to add link to visualizer in button!!*/}
-                      <button
-                        type="button"
-                        className="rounded bg-indigo-600 py-1 px-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        View Results
-                      </button>
-                    </td>
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-{/* need to add link to visualizer in button!!*/}
-                      <button
-                        type="button"
-                        className="rounded bg-indigo-600 py-1 px-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        Repeat Experiment
-                      </button>
-                    </td>
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
-                      <button
-                        type="button"
-                        className="rounded bg-indigo-600 py-1 px-2 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        onClick={() => handleDelete(featureObj.id, pastFeatures, setpastFeatures, setError)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+              ))}
+            </tbody>
+          </table>
             }
           </div>
         </div>
