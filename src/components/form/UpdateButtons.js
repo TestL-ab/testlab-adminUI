2f24a441547e90c0d961bab04b0e02c7420c9749
ChelseaSaunders <c.c.saunders@protonmail.com>
2023-03-15 16:00:28 -0700

    fixed spacing in input boxes and variantupdate bug Co-authored-by: Alison Martinez <alison.a.martinez@outlook.com> Co-authored-by: apapka <abbiepapka@gmail.com> Co-authored-by: Sarah Bunker <chemencoder@gmail.com>

--- src/components/form/UpdateButtons.js
+++ src/components/form/UpdateButtons.js
@@ -10,7 +10,7 @@ const UpdateButtons = ({
     <>
       <button
         type="submit"
-        className="ml-3 inline-flex justify-center rounded-md bg-testLabBlue py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ml-0 mr-3"
+        className="ml-0 inline-flex justify-center rounded-md bg-testLabBlue py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ml-0 mr-3"
       >
         Update Feature
       </button>
