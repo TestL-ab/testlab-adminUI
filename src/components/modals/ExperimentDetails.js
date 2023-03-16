2640ad9896ea8d036fd4439f3d2950dbc03466e4
ChelseaSaunders <c.c.saunders@protonmail.com>
2023-03-16 11:16:48 -0700

    still tweaking experiment details Co-authored-by: Alison Martinez <alison.a.martinez@outlook.com> Co-authored-by: apapka <abbiepapka@gmail.com> Co-authored-by: Sarah Bunker <chemencoder@gmail.com>

--- src/components/modals/ExperimentDetails.js
+++ src/components/modals/ExperimentDetails.js
@@ -10,61 +10,58 @@ const ExperimentDetails = ({ experiment, controlVariant, otherVariants, dispatch
       <div className="sm:col-span-8 lg:col-span-7 px-4 py-4">
         <div className="overflow-hidden bg-white shadow sm:rounded-lg">
           <div className="px-4 py-5 sm:px-6">
-            <h3 className="text-base font-semibold leading-6 text-gray-900">{experiment.name}</h3>
+            <h3 className="text-lg font-semibold leading-6 text-gray-900">{experiment.name}</h3>
             <p className="mt-1 max-w-2xl text-sm text-gray-500">{experiment.description}</p>
           </div>
           <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
             <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
               <div className="sm:col-span-1">
-                <dt className="text-sm font-medium text-gray-500">Start Date</dt>
-                <dd className="mt-1 text-sm text-gray-900">{experiment.startDate}</dd>
+                <dt className="text-s font-medium text-gray-500">Start Date</dt>
+                <dd className="mt-1 text-s text-gray-900">{experiment.startDate}</dd>
               </div>
               <div className="sm:col-span-1">
-                <dt className="text-sm font-medium text-gray-500">End Date</dt>
-                <dd className="mt-1 text-sm text-gray-900">{experiment.endDate}</dd>
+                <dt className="text-s font-medium text-gray-500">End Date</dt>
+                <dd className="mt-1 text-s text-gray-900">{experiment.endDate}</dd>
               </div>
               <div className="sm:col-span-1">
-                <dt className="text-sm font-medium text-gray-500">Users Enrolled</dt>
-                <dd className="mt-1 text-sm text-gray-900">{experiment.userPercentage}</dd>
+                <dt className="text-s font-medium text-gray-500">Users Enrolled</dt>
+                <dd className="mt-1 text-s text-gray-900">{experiment.userPercentage}</dd>
               </div>
               <div className="sm:col-span-2">
                 <div>
-                  <h3 className="text-base font-semibold leading-6 text-gray-900">Variants:</h3>
+                  <h3 className="text-s font-medium text-gray-500">Variants:</h3>
+                  <br />
 
 
 
                   <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                     <li key={controlVariant.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
                       <div className="flex w-full items-center justify-between space-x-6 p-6">
-                        <div className="flex-1 truncate">
+                        <div className="flex-1">
 
                           <div className="flex items-center space-x-3">
 
-                            <h2 className="text-s font-bold break-normal text-gray-900">{controlVariant.value}</h2>
+                            <p className="text-sm font-bold break-normal text-gray-900">{controlVariant.value}</p>
                           </div>
                           <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800">
                               Control
                             </span>
                             <br/>
                             <br/>
-                          <p className="mt-1 truncate text-sm text-gray-500">{controlVariant.weight * 100}% of users in experiment</p>
-
-
+                          <p className="mt-1 text-sm text-gray-500">{controlVariant.weight * 100}% of users in experiment</p>
                         </div>
-
                       </div>
                     </li>
                     {otherVariants.map((variant) => (
                       <li key={variant.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow">
                         <div className="flex w-full items-center justify-between space-x-6 p-6">
-                          <div className="flex-1 truncate">
+                          <div className="flex-1">
                             <div className="flex items-center space-x-3">
-                              <h3 className="text-s font-bold text-gray-900">{variant.value}</h3>
+                              <h3 className="text-sm font-bold text-gray-900">{variant.value}</h3>
                             </div>
                             <br/>
-
                             <br/>
-                            <p className="mt-1 truncate text-sm text-gray-500">{variant.weight * 100}% of users in experiment</p>
+                            <p className="mt-1 text-sm text-gray-500">{variant.weight * 100}% of users in experiment</p>
                           </div>
                         </div>
                       </li>
@@ -74,7 +71,7 @@ const ExperimentDetails = ({ experiment, controlVariant, otherVariants, dispatch
               </div>
               {futureExperiment ? null : <button
                 type="button"
-                className="rounded-full bg-testLabBlue w-32 h-8 py-1 px-2.5 text-xs font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-testLabBlue"
+                className="rounded-full bg-testLabBlue w-32 h-8 py-1 px-2.5 text-sm font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-testLabBlue"
                 onClick={handleClick}
               >
                 View Analytics
