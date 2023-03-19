8e3a47433f96abc6d6b4f7f8105375efe2d15024
apapka <50668316+apapka@users.noreply.github.com>
2023-03-18 20:48:12 -0700

    undo that :)
    Co-authored-by: Alison Adams Martinez <101058073+alison-martinez@users.noreply.github.com>
    Co-authored-by: Chelsea Saunders <68711819+ChelseaSaunders@users.noreply.github.com>
    Co-authored-by: Sarah Bunker <79855628+SarahBunker@users.noreply.github.com>

--- src/components/form/UpdateButtons.js
+++ src/components/form/UpdateButtons.js
@@ -10,7 +10,7 @@ const UpdateButtons = ({
     <div className="flex justify-start">
       <button
         type="submit"
-        className="ml-0 inline-flex justify-center rounded-md bg-testLabBlue py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-testLabDarkBlue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ml-0 mr-3"
+        className="ml-3 inline-flex justify-center rounded-md bg-testLabBlue py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-testLabBlue"
       >
         Update Feature
       </button>
@@ -20,7 +20,6 @@ const UpdateButtons = ({
         setShowVariants={setShowVariants}
       />
     </div>
-
   );
 };
 
