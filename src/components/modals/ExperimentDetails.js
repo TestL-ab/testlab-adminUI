cca3869bc12450664c2f714c2446528f80f5250e
apapka <50668316+apapka@users.noreply.github.com>
2023-03-20 13:13:00 -0700

    add link to docs
    Co-authored-by: Alison Adams Martinez <101058073+alison-martinez@users.noreply.github.com>
    Co-authored-by: Chelsea Saunders <68711819+ChelseaSaunders@users.noreply.github.com>
    Co-authored-by: Sarah Bunker <79855628+SarahBunker@users.noreply.github.com>

--- src/components/modals/ExperimentDetails.js
+++ src/components/modals/ExperimentDetails.js
@@ -11,7 +11,7 @@ const ExperimentDetails = ({ experiment, controlVariant, otherVariants, dispatch
         <div className="overflow-hidden bg-white sm:rounded-lg">
           <div className="px-4 py-5 sm:px-6">
             <h1 className="text-2xl font-extrabold leading-6 text-gray-900">{experiment.name}</h1>
-            <p className="pt-2 mt-1 max-w-2xl text-sm text-gray-500">Experiment Description: {experiment.description}</p>
+            <p className="pt-2 leading-relaxed mt-1 italic tracking-tight max-w-2xl text-descr text-gray-500">Experiment Description: {experiment.description}</p>
           </div>
           <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
             <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-4">
