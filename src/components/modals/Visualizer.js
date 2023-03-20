87f89b96420eb0279a9da07ed077c8bd042987cd
apapka <50668316+apapka@users.noreply.github.com>
2023-03-20 12:59:14 -0700

    add hover info to other charts
    Co-authored-by: Alison Adams Martinez <101058073+alison-martinez@users.noreply.github.com>
    Co-authored-by: Chelsea Saunders <68711819+ChelseaSaunders@users.noreply.github.com>
    Co-authored-by: Sarah Bunker <79855628+SarahBunker@users.noreply.github.com>

--- src/components/modals/Visualizer.js
+++ src/components/modals/Visualizer.js
@@ -22,57 +22,66 @@ export default function Visualizer({ experiment, handleClick, featureAnalysis, e
         </div>
         :
         <>
-            <div className="overflow-hidden bg-white shadow sm:rounded-lg ">
-              <div className="px-4 py-5 sm:px-6 ">
-                <h1 className="text-xl font-semibold leading-6 text-gray-900">{experiment.name}</h1>
-                <p className="mt-1 max-w-2xl text-sm text-gray-500">{experiment.description}</p>
-              </div>
-              <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
-                <div className='container  '>
-                  <div className='border-b border-gray-200 px-4 py-5 sm:px-6'>
-                    <div>
-                      <h1 className="flex items-center">
-                        Normalized Event Data
-                        <HoverInfo featureAnalysis={featureAnalysis} />
-                      </h1>
-                      <p className='text-gray-500 text-sm font-style: italic'>Total events corrected for percentage of users in each variant</p>
-                      <div className="grid justify-center pt-4">
-                        <WeightedBarChart featureAnalysis={featureAnalysis} />
-                      </div>
-                    </div>
-                  </div>
-                  <div className='border-b border-gray-200 px-4 py-5 sm:px-6'>
-                    <h1>Raw Event Data</h1>
-                    <p className='text-gray-500 text-sm font-style: italic'>Total of all event data, split based on distinct users</p>
+          <div className="overflow-hidden bg-white shadow sm:rounded-lg ">
+            <div className="px-4 py-5 sm:px-6 ">
+              <h1 className="text-xl font-semibold leading-6 text-gray-900">{experiment.name}</h1>
+              <p className="mt-1 max-w-2xl text-sm text-gray-500">{experiment.description}</p>
+            </div>
+            <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+              <div className='container  '>
+                <div className='border-b border-gray-200 px-4 py-5 sm:px-6'>
+                  <div>
+                    <h1 className="flex items-center">
+                      Normalized Event Data
+                      <HoverInfo featureAnalysis={featureAnalysis} />
+                    </h1>
+                    <p className='text-gray-500 text-sm font-style: italic'>Total events corrected for percentage of users in each variant</p>
                     <div className="grid justify-center pt-4">
-
-                    <SimpleBarChart featureAnalysis={featureAnalysis} />
+                      <WeightedBarChart featureAnalysis={featureAnalysis} />
                     </div>
-                  </div>
-                  <div className='border-b border-gray-200 px-4 py-5 mb-4 sm:px-6'>
-                    <h1>Timeline of Event Data</h1>
-                    <p className='text-gray-500 text-sm font-style: italic'>All event data displayed by date</p>
-                    <div className="grid justify-center pt-4">
-
-                    <DailyLineChart eventData={eventData} featureAnalysis={featureAnalysis} />
-                    </div>
-
                   </div>
                 </div>
-                <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-                  <button
-                    type="button"
-                    className="rounded-full bg-testLabBlue py-1 px-2.5 text-xs font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-testLabBlue"
-                    onClick={handleClick}
-                  >
-                    <div className="flex items-center text-sm">
-                      <ArrowLeftCircleIcon className="h-6 w-6 mr-2" aria-hidden="true" /> 
-                      Return to Experiment Details
-                    </div>
-                  </button>
-                </dl>
+                <div className='border-b border-gray-200 px-4 py-5 sm:px-6'>
+                  <div>
+                    <h1 className="flex items-center">
+                      Raw Event Data
+                      <HoverInfo featureAnalysis={featureAnalysis} />
+                    </h1>
+                  </div>
+                  <p className='text-gray-500 text-sm font-style: italic'>Total of all event data, split based on distinct users</p>
+                  <div className="grid justify-center pt-4">
+                    <SimpleBarChart featureAnalysis={featureAnalysis} />
+                  </div>
+                </div>
+                <div className='border-b border-gray-200 px-4 py-5 mb-4 sm:px-6'>
+                  <div>
+                  <h1 className="flex items-center">
+                    Timeline of Event Data
+                    <HoverInfo featureAnalysis={featureAnalysis} />
+                  </h1>
+                  </div>
+                  <p className='text-gray-500 text-sm font-style: italic'>All event data displayed by date</p>
+                  <div className="grid justify-center pt-4">
+
+                    <DailyLineChart eventData={eventData} featureAnalysis={featureAnalysis} />
+                  </div>
+
+                </div>
               </div>
+              <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                <button
+                  type="button"
+                  className="rounded-full bg-testLabBlue py-1 px-2.5 text-xs font-semibold text-white shadow-sm hover:bg-testLabBeige focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-testLabBlue"
+                  onClick={handleClick}
+                >
+                  <div className="flex items-center text-sm">
+                    <ArrowLeftCircleIcon className="h-6 w-6 mr-2" aria-hidden="true" />
+                    Return to Experiment Details
+                  </div>
+                </button>
+              </dl>
             </div>
+          </div>
         </>}
     </>
   )
