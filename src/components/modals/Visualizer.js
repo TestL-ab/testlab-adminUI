c23457d8c0a8882e87b87c19b08897e2aba29d2f
apapka <50668316+apapka@users.noreply.github.com>
2023-03-19 23:24:45 -0700

    headings bolder
    Co-authored-by: Alison Adams Martinez <101058073+alison-martinez@users.noreply.github.com>
    Co-authored-by: Chelsea Saunders <68711819+ChelseaSaunders@users.noreply.github.com>
    Co-authored-by: Sarah Bunker <79855628+SarahBunker@users.noreply.github.com>

--- src/components/modals/Visualizer.js
+++ src/components/modals/Visualizer.js
@@ -25,7 +25,7 @@ export default function Visualizer({ experiment, handleClick, featureAnalysis, e
           <div className="sm:col-span-8 lg:col-span-7 px-4 py-4 ">
             <div className="overflow-hidden bg-white shadow sm:rounded-lg ">
               <div className="px-4 py-5 sm:px-6 ">
-                <h3 className="text-base font-semibold leading-6 text-gray-900">{experiment.name}</h3>
+                <h1 className="text-xl font-semibold leading-6 text-gray-900">{experiment.name}</h1>
                 <p className="mt-1 max-w-2xl text-sm text-gray-500">{experiment.description}</p>
               </div>
               <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
